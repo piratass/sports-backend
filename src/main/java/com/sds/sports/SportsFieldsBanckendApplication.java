81b9b1a43dc521026742cec4e63f769ae4a4797a
Cristhian Ramiro Cerpa Gallegos <externo371@compartamos.pe>
2023-09-12 00:03:38 -0500

    init

---
+++ src/main/java/com/sds/sports/SportsFieldsBanckendApplication.java
@@ -0,0 +1,12 @@
+package com.sds.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SportsFieldsBanckendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SportsFieldsBanckendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sds/sports/SportsFieldsBanckendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sds.sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SportsFieldsBanckendApplicationTests {
+
+  @Test void contextLoads() {
+  }
+
+}
